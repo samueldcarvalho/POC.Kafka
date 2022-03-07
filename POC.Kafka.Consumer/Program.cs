eb20355837004cdabaf7e318ce084f890a7817e0
samueldcarvalho <samueldcarvalho99@gmail.com>
2022-03-07 15:48:37 -0300

    Criação dos projetos Console: Consumer e Producer

---
+++ POC.Kafka.Consumer/Program.cs
@@ -0,0 +1,50 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+namespace POC.Kafka.Consumer
+{
+    class Program
+    {
+        static void Main()
+        {
+            var conf = new ConsumerConfig
+            {
+                GroupId = "test-consumer-group",
+                BootstrapServers = "127.0.0.1:9092",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
+
+            using (var c = new ConsumerBuilder<Ignore, string>(conf).Build())
+            {
+                c.Subscribe("test-topic");
+
+                var cts = new CancellationTokenSource();
+                Console.CancelKeyPress += (_, e) => {
+                    e.Cancel = true;
+                    cts.Cancel();
+                };
+
+                try
+                {
+                    while (true)
+                    {
+                        try
+                        {
+                            var cr = c.Consume(cts.Token);
+                            Console.WriteLine($"Consumed message '{cr.Value}' at: '{cr.TopicPartitionOffset}'.");
+                        }
+                        catch (ConsumeException e)
+                        {
+                            Console.WriteLine($"Error occured: {e.Error.Reason}");
+                        }
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    c.Close();
+                }
+            }
+        }
+    }
+}
---
+++ src/POC.Kafka/POC.Kafka.Source/Program.cs
@@ -0,0 +1,35 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace POC.Kafka.Source
+{
+    class Program
+    {
+        static async Task Main()
+        {
+            var config = new ProducerConfig { BootstrapServers = "localhost:9092" };
+
+            using (var p = new ProducerBuilder<Null, string>(config).Build())
+            {
+                try
+                {
+                    var count = 0;
+                    while (true)
+                    {
+                        count++;
+                        var dr = await p.ProduceAsync("test-topic", new Message<Null, string> { Value = $"TESTE DE MENSAGEM: Nº{count}" });
+                        Console.WriteLine($"Delivered '{dr.Value}' to '{dr.TopicPartitionOffset}' | {count}");
+
+                        Thread.Sleep(4000);
+                    }
+                }
+                catch (ProduceException<Null, string> e)
+                {
+                    Console.WriteLine($"Delivery failed: {e.Error.Reason}");
+                }
+            }
+        }
+    }
+}
